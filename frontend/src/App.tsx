89ee51c28307b119d9a51b23d9c9deb6eab21f99
Anton Florendo <antonflorendo7@gmail.com>
2026-03-08 04:17:05 -0700

    feat(01-02): wrap App.tsx with ThemeProvider as outermost element
    - ThemeProvider now wraps BrowserRouter in App.tsx
    - Added eslint-disable for react-refresh/only-export-components on useTheme hook
    - ThemeProvider is outermost so all route components can call useTheme()

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProjectsPage from "./pages/ProjectsPage";
 import Layout from "./Layout";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="projects" element={<ProjectsPage />} />
-          <Route path="contact" element={<Navigate to="/#contact" replace />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="projects" element={<ProjectsPage />} />
+            <Route path="contact" element={<Navigate to="/#contact" replace />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
